1dedd0fd1312d6beb91d4f785565ae9d80840eb3
Bartosz Ciach <bartosz.ciach@tooploox.com>
2023-12-11 23:14:13 +0100

    sum stuff about day 11

--- 2023/day11/index.js
+++ 2023/day11/index.js
@@ -6,8 +6,8 @@ let observable = (await Bun.file('./input.txt').text())
 function transpose(mat) {
   const tMat = Array.from(Array(mat[0].length)).map(() => Array(mat[0].length));
 
-  for (let i = 0; i < mat.length; ++i)
-    for (let j = 0; j < mat[i].length; ++j) tMat[j][i] = mat[i][j];
+  for (let y = 0; y < mat.length; ++y)
+    for (let x = 0; x < mat[y].length; ++x) tMat[x][y] = mat[y][x];
 
   return tMat;
 }
@@ -19,16 +19,16 @@ function expand(mat) {
 }
 
 function calcDistance(a, b, mat, mul) {
-  const [iA, jA] = a;
-  const [iB, jB] = b;
+  const [yA, xA] = a;
+  const [yB, xB] = b;
 
-  const horStart = Math.min(jA, jB);
-  const horEnd = Math.max(jA, jB);
+  const horStart = Math.min(xA, xB);
+  const horEnd = Math.max(xA, xB);
 
-  let horizontalPath = mat[iA].slice(horStart, horEnd);
+  let horizontalPath = mat[yA].slice(horStart, horEnd);
 
-  const verStart = Math.min(iA, iB);
-  const verEnd = Math.max(iA, iB);
+  const verStart = Math.min(yA, yB);
+  const verEnd = Math.max(yA, yB);
 
   let verticalPath = [];
   for (let i = verStart; i < verEnd; ++i) verticalPath.push(mat[i][horEnd]);
@@ -38,13 +38,13 @@ function calcDistance(a, b, mat, mul) {
 
 let expanded = expand(transpose(expand(observable)));
 
-// [i,j] coords of every galaxy
-let galaxies = expanded.flatMap((l, i) =>
-  l.map((g, j) => (g === '#' ? [i, j] : undefined)).filter(Boolean),
+let galaxies = expanded.flatMap((l, y) =>
+  l.map((g, x) => (g === '#' ? [y, x] : undefined)).filter(Boolean),
 );
 
 let sky = galaxies.reduce(
   (s, g, i, a) => a.slice(i + 1).reduce((s, g_) => s + calcDistance(g, g_, expanded, 1_000_000), s),
   0,
 );
+
 console.log(sky);
