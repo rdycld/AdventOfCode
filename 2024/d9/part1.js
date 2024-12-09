6297e459977c4a76aaa93690d3682293b2a8f63a
Bartosz Ciach <bartoszciach99@gmail.com>
2024-12-09 10:22:06 +0100

    d9p1

---
+++ 2024/d9/part1.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("./input.txt", { encoding: "utf8" });
+
+const data = input.split("").map(Number);
+
+const disc = [];
+
+for (let i = 0; i < data.length; ++i) 
+  for (let c = data[i]; c > 0; --c) disc.push(i % 2 === 0 ? i / 2 : ".");
+
+while (disc.includes(".")) {
+  disc[disc.indexOf(".")] = disc.pop();
+
+  while (disc[disc.length - 1] === ".") disc.pop();
+}
+
+const p1 = disc.reduce((p, v, i) => p + v * i);
+
+console.log(p1);
