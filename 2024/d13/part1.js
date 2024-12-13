ebb33bf0f1a6bbd264fe2b4bbf443b8fbb0ead1c
Bartosz Ciach <bartoszciach99@gmail.com>
2024-12-13 14:05:58 +0100

    fixes

--- 2024/d13/part1.js
+++ 2024/d13/part1.js
@@ -1,41 +1,41 @@
 const fs = require("fs");
 
-// Button A: X+94, Y+34
-// Button B: X+22, Y+67
-// Prize: X=8400, Y=5400
-
 /**
- *  mA + nB = C (m=94, n=22, C = 8400)
- *  oA + pB = D (o=34, p,=67, D = 8400)
+ *  Button A: X+94, Y+34
+ *  Button B: X+22, Y+67
+ *  Prize: X=8400, Y=5400
  *
- *  mA +nB = C
+ *  mA + oB = C (m=94, o=22, C = 8400)
+ *  nA + pB = D (n=34, p,=67, D = 8400)
  *
- *  mA = C - nB
+ *  mA +oB = C
  *
- *  A = (C - nB)/m
+ *  mA = C - oB
  *
- *  o(C - nB)/m + pB = D
+ *  A = (C - oB)/m
  *
- *  oC/m - onB/m + pB = D
+ *  n(C - oB)/m + pB = D
  *
- * oC - onB + mpB = Dm
+ *  nC/m - noB/m + pB = D
  *
- * mpB - onB = Dm - oC
+ * nC - onB + mpB = Dm
  *
- * B(mp - on) = Dm - oC
+ * mpB - onB = Dm - nC
  *
- * B = (Dm - oC)/ (mp - on)
+ * B(mp - on) = Dm - nC
+ *
+ * B = (Dm - nC)/ (mp - on)
  */
 
 console.time("part1");
 const input = fs.readFileSync("./input.txt", { encoding: "utf8" });
 
 const part1 = input.split("\n\n").reduce((acc, machine) => {
-  const [,m,n,o,p,C,D]  = machine.split(/\D+/)
-  let B = (+D * +m - +n * +C) / (+p * +m - +n * +o);
-  if(B % 1 !==0) return acc
-  return acc + 3* ((+C - +o * +B) / +m) + B
+  const [, m, n, o, p, C, D] = machine.split(/\D+/).map(Number);
+  let B = (D * m - n * C) / (p * m - n * o);
+  if (B % 1 !== 0) return acc;
+  return acc + 3 * ((C - o * B) / m) + B;
 }, 0);
 
-console.log(part1)
+console.log(part1);
 console.timeEnd("part1");
