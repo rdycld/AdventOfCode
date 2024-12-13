3a3d8f34835af2081ca0e89a5021f9755956fb2d
Bartosz Ciach <bartoszciach99@gmail.com>
2024-12-13 20:45:15 +0100

    add math behind solution

--- 2024/d13/part1.js
+++ 2024/d13/part1.js
@@ -3,6 +3,21 @@ const fs = require("fs");
 console.time("part1");
 const input = fs.readFileSync("./input.txt", { encoding: "utf8" });
 
+/**
+ * Button A: X+94, Y+34
+   Button B: X+22, Y+67
+   Prize: X=8400, Y=5400
+
+   mA + oB = C (m = 94, o = 22, C = 8400)
+   nA + pB = D (n = 34, p = 67, D = 5400);
+
+   mA = c - oB --> A = (C - oB)/m
+
+   n(C - oB)/m + pB = D --> nC/m - noB/m + pB = D --> nC - noB + mpB = Dm
+
+   mpB + noB = Dm - nC --> B(mp + no)= Dm - nC --> B = (Dm - nC)/ (mp + no)
+ */
+
 const part1 = input.split("\n\n").reduce((acc, machine) => {
   const [, m, n, o, p, C, D] = machine.split(/\D+/).map(Number);
   let B = (D * m - n * C) / (p * m - n * o);
