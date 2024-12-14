39468f2113ddb535f0c153dacbd1d5945abedec0
Bartosz Ciach <bartoszciach99@gmail.com>
2024-12-14 14:06:27 +0100

    fix d14p1

--- 2024/d14/part1.js
+++ 2024/d14/part1.js
@@ -17,36 +17,30 @@ const robots = input.split("\n").map((robot) => {
   };
 });
 
-fs.writeFileSync('./foo.txt', JSON.stringify(robots))
 
-console.log(robots)
-
-// let height = 103;
-// let width = 101;
-// let t = 100;
+let height = 103;
+let width = 101;
+let t = 100;
 
 
-//   let topRight = 0;
-//   let topLeft = 0;
-//   let botRight = 0;
-//   let botLeft = 0;
-//   for (let r of robots) {
-//     let x = (r.sx + t * r.vx) % width;
-//     let y = (r.sy + t * r.vy) % height;
+  let topRight = 0;
+  let topLeft = 0;
+  let botRight = 0;
+  let botLeft = 0;
+  for (let r of robots) {
+    let x = (r.sx + t * r.vx) % width;
+    let y = (r.sy + t * r.vy) % height;
 
-//     let _x = x < 0 ? width + x : x;
-//     let _y = y < 0 ? height + y : y;
+    let _x = x < 0 ? width + x : x;
+    let _y = y < 0 ? height + y : y;
 
-//     if (_y < Math.floor(height / 2) && _x < Math.floor(width / 2)) topLeft += 1;
-//     if (_y > Math.floor(height / 2) && _x < Math.floor(width / 2)) botLeft += 1;
-//     if (_y < Math.floor(height / 2) && _x > Math.floor(width / 2))
-//       topRight += 1;
-//     if (_y > Math.floor(height / 2) && _x > Math.floor(width / 2))
-//       botRight += 1;
-//   }
+    if (_y < Math.floor(height / 2) && _x < Math.floor(width / 2)) topLeft += 1;
+    if (_y > Math.floor(height / 2) && _x < Math.floor(width / 2)) botLeft += 1;
+    if (_y < Math.floor(height / 2) && _x > Math.floor(width / 2))
+      topRight += 1;
+    if (_y > Math.floor(height / 2) && _x > Math.floor(width / 2))
+      botRight += 1;
+  }
 
-//   let v = topRight * topLeft * botRight * botLeft;
-
-
-
-// console.log(v);
+  let v = topRight * topLeft * botRight * botLeft;
+console.log(v);
