0d6ca2bc4632307a7ae1c6761c3ff17108cdccee
Michał Papliński <paplinski.michal@gmail.com>
2024-12-15 22:59:46 +0100

    Update part2.js

--- 2024/d15/part2.js
+++ 2024/d15/part2.js
@@ -1,106 +1,74 @@
-const fs = require("fs");
-
 console.time("part2");
-const input = fs.readFileSync("./input.txt", { encoding: "utf8" });
-
-let [grid, instructions] = [
-  input
-    .split("\n\n")[0]
-    .replaceAll("#", "##")
-    .replaceAll("O", "[]")
-    .replaceAll(".", "..")
-    .replace("@", "@.")
-    .split("\n")
-    .map((l) => l.split("")),
-  input.split("\n\n")[1].split("\n").join(""),
-];
-
-let directions = {
-  "^": [-1, 0],
-  ">": [0, 1],
-  "<": [0, -1],
-  v: [1, 0],
-};
-
+let input = require("fs").readFileSync("./input.txt", "utf8").split("\n\n");
+let grid = input[0]
+  .replaceAll("#", "##")
+  .replaceAll("O", "[]")
+  .replaceAll(".", "..")
+  .replace("@", "@.")
+  .split("\n")
+  .map((l) => l.split(""));
+let instructions = input[1].split("\n").join("");
 let y;
 let x;
-for (let _y = 0; _y < grid.length; ++_y)
-  for (let _x = 0; _x < grid[0].length; ++_x)
+let seen = new Set();
+
+for (let _y = 0; _y < grid.length; _y++)
+  for (let _x = 0; _x < grid[0].length; _x++)
     if (grid[_y][_x] === "@") {
       y = _y;
       x = _x;
     }
 
-function keyGen(y, x) {
-  return `${y}-${x}`;
-}
-
-let seen = new Set();
-
-function lookAhead(y, x, dir) {
-  let halfA = grid[y][x];
-  let halfBX = x + (halfA === "]" ? -1 : +1);
-
-  seen.add(keyGen(y, x));
-  seen.add(keyGen(y, halfBX));
-
-  let upA = grid[y + dir][x];
-  let upB = grid[y + dir][halfBX];
-
+function lookAhead(Y, X, dir) {
+  let halfA = grid[Y][X];
+  let halfBX = X + (halfA === "]" ? -1 : 1);
+  seen.add(Y + "-" + X);
+  seen.add(Y + "-" + halfBX);
+  let upA = grid[Y + dir][X];
+  let upB = grid[Y + dir][halfBX];
   if (upA === "." && upB === ".") {
-    seen.add(keyGen(y + dir, x));
-    seen.add(keyGen(y + dir, halfBX));
+    seen.add(Y + dir + "-" + X);
+    seen.add(Y + dir + "-" + halfBX);
     return true;
   }
   if (upA === "#" || upB === "#") return false;
-
   if (upA === ".") {
-    seen.add(keyGen(y + dir, x));
-
-    return lookAhead(y + dir, halfBX, dir);
+    seen.add(Y + dir + "-" + X);
+    return lookAhead(Y + dir, halfBX, dir);
   }
-
   if (upB === ".") {
-    seen.add(keyGen(y + dir, halfBX));
-    return lookAhead(y + dir, x, dir);
+    seen.add(Y + dir + "-" + halfBX);
+    return lookAhead(Y + dir, X, dir);
   }
-
-  return lookAhead(y + dir, x, dir) && lookAhead(y + dir, halfBX, dir);
+  return lookAhead(Y + dir, X, dir) && lookAhead(Y + dir, halfBX, dir);
 }
 
 for (let i of instructions) {
-  let [dy, dx] = directions[i];
+  let dy = i === "^" ? -1 : i === "v" ? 1 : 0;
+  let dx = i === ">" ? 1 : i === "<" ? -1 : 0;
   let ny = y + dy;
   let nx = x + dx;
 
-  let next = grid[ny][nx];
-
-  if (next === ".") {
+  if (grid[ny][nx] === ".") {
     grid[y][x] = ".";
     grid[ny][nx] = "@";
     y = ny;
     x = nx;
     continue;
   }
-
-  if (next === "#") continue;
+  if (grid[ny][nx] === "#") continue;
 
   if (i === "<" || i === ">") {
     let fy = ny;
     let fx = nx;
-
     while (true) {
       fy += dy;
       fx += dx;
-      let following = grid[fy][fx];
-
-      if (following === "[" || following === "]") continue;
-      if (following === "#") break;
-
+      if (grid[fy][fx] === "[" || grid[fy][fx] === "]") continue;
+      if (grid[fy][fx] === "#") break;
       let slice = grid[y].slice(Math.min(x, fx - dx), Math.max(x, fx - dx) + 1);
       if (i === "<") slice.push(".");
       else slice.unshift(".");
-
       grid[y].splice(Math.min(x, fx), slice.length, ...slice);
       y = ny;
       x = nx;
@@ -109,21 +77,15 @@ for (let i of instructions) {
   }
 
   if ((i === "^" || i === "v") && lookAhead(ny, nx, dy)) {
-    let points = Array.from(seen).sort((a, b) => {
-      let [yA] = a.split("-").map(Number);
-      let [yB] = b.split("-").map(Number);
-
+    for (let p of Array.from(seen).sort((a, b) => {
+      let yA = +a.split("-")[0];
+      let yB = +b.split("-")[0];
       return dy * (yB - yA);
-    });
-
-    for (let point of points) {
-      let [y, x] = point.split("-").map(Number);
-
-      let isNextInSeen = seen.has(keyGen(y - dy, x));
-
-      grid[y][x] = isNextInSeen ? grid[y - dy][x] : ".";
+    })) {
+      let Y = +p.split("-")[0];
+      let X = +p.split("-")[1];
+      grid[Y][X] = seen.has(Y - dy + "-" + X) ? grid[Y - dy][X] : ".";
     }
-
     grid[ny][nx] = "@";
     grid[y][x] = ".";
     y = ny;
@@ -133,10 +95,9 @@ for (let i of instructions) {
 }
 
 let sum = 0;
-
-for (let y = 0; y < grid.length; ++y)
-  for (let x = 0; x < grid[0].length; ++x)
-    if (grid[y][x] === "[") sum += 100 * y + x;
+for (let Y = 0; Y < grid.length; Y++)
+  for (let X = 0; X < grid[0].length; X++)
+    if (grid[Y][X] === "[") sum += 100 * Y + X;
 
 console.log(sum);
 console.timeEnd("part2");
