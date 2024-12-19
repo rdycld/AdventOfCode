f3777b64e83b51ab3aed3c3e084394dc2732f94c
Bartosz Ciach <bartoszciach99@gmail.com>
2024-12-19 22:56:23 +0100

    cleanup

--- 2024/d17/part2.js
+++ 2024/d17/part2.js
@@ -5,78 +5,70 @@ console.time("part2");
 // const digits = [ 7,0,2,6,4,2,5,2,3,6,5,1,4,2,7,2].reverse();
 // found it manually :>
 
-let calcInput = (digits) => {
+let octArrToDec = (digits) => {
+  let d = digits.toReversed();
   let v = 0;
-  for (let i = 0; i < digits.length; ++i) {
-    v += digits[i] * 8 ** i;
+  for (let i = 0; i < d.length; ++i) {
+    v += d[i] * 8 ** i;
   }
   return BigInt(v);
 };
 
 let registerA = 0n;
-let registerB = BigInt(0);
-let registerC = BigInt(0);
+let registerB = 0n;
+let registerC = 0n;
 const program = [2, 4, 1, 1, 7, 5, 4, 0, 0, 3, 1, 6, 5, 5, 3, 0];
 
 const getCombo = (operand) => {
-  if (operand < 4) return operand;
+  if (operand < 4) return BigInt(operand);
   if (operand === 4) return registerA;
   if (operand === 5) return registerB;
   if (operand === 6) return registerC;
 
-  throw "dupa";
+  throw "err";
 };
 
-let buff = "";
+let buffer = "";
 
 let iP = 0;
-const foo = {
+const functions = {
   0: (op) => {
-    registerA = BigInt(registerA / BigInt(BigInt(2) ** BigInt(getCombo(op))));
+    registerA /= 2n ** getCombo(op);
   },
   1: (op) => {
     registerB ^= BigInt(op);
   },
   2: (op) => {
-    registerB = BigInt(getCombo(op) % BigInt(8));
+    registerB = getCombo(op) % 8n;
   },
   3: (op) => {
-    if (registerA) {
-      iP = op - 2;
-    }
+    if (registerA) iP = op - 2;
   },
   4: () => {
     registerB ^= registerC;
   },
   5: (op) => {
-    buff += getCombo(op) % BigInt(8);
+    buffer += getCombo(op) % 8n;
   },
   6: (op) => {
-    registerB = BigInt(registerA / BigInt(BigInt(2) ** BigInt(getCombo(op))));
+    registerB = registerA / 2n ** BigInt(getCombo(op));
   },
   7: (op) => {
-    registerC = BigInt(registerA / BigInt(BigInt(2) ** BigInt(getCombo(op))));
+    registerC = registerA / BigInt(2n ** getCombo(op));
   },
 };
 
 function run() {
-  iP = 0;
-  for (; iP < program.length - 1; iP += 2) {
-    let func = program[iP];
-    foo[func](program[iP + 1]);
-  }
+  for (iP = 0; iP < program.length - 1; iP += 2)
+    functions[program[iP]](program[iP + 1]);
 
-  let b = buff;
-  buff = "";
+  let b = buffer;
+  buffer = "";
   return b;
 }
 
 let digits = [];
-
 let desiredOutput = program.join("");
-
-console.log(desiredOutput);
-
 let backtrack = false;
 
 main: while (true) {
@@ -85,26 +77,25 @@ main: while (true) {
 
   while (true) {
     digits[digits.length - 1] += 1;
+
     if (digits[digits.length - 1] === 8) {
       digits.pop();
       backtrack = true;
       break;
     }
 
-    registerA = calcInput(digits.toReversed());
+    registerA = octArrToDec(digits);
+
     let output = run();
 
     if (desiredOutput === output) {
-      console.log("found");
-      console.log(calcInput(digits.toReversed()));
+      console.log('found: ',octArrToDec(digits));
       break main;
     }
 
-    if (output.length && desiredOutput.endsWith(output)) {
-      break;
-    }
+    console.log(digits);
+    if (output.length && desiredOutput.endsWith(output)) break;
   }
-  console.log(digits);
 }
 
 console.timeEnd("part2");
